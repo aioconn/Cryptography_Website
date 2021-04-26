1d610a3aee1a7dc19b350238937b628a2894c531
Jarret Whitehouse <55901610+JWhitehouse06@users.noreply.github.com>
2021-04-26 16:35:41 -0400

    Update cryptoweb_Tools.js

--- cryptoweb_Tools.js
+++ cryptoweb_Tools.js
@@ -5,6 +5,8 @@
 //                  2021                   //
 /////////////////////////////////////////////
 
+
+
 function freqAnalysis(message, segmentLength) { 
 
     let output = [];
@@ -12,7 +14,7 @@ function freqAnalysis(message, segmentLength) {
     //get the segments for count
     for(let i = 0; i <= message.length - segmentLength; i++){
 
-        var segment = message.substr(i, segmentLength);
+        let segment = message.substr(i, segmentLength);
 
         //see if it is in the list
         let index = -1;
@@ -86,6 +88,235 @@ function sortFrequenciesProportion(frequencyList){
     
 }
 
+//calculate single frequencies and build graph
+
+function updateGraph(){
+	
+	//sanitize input
+	let ciphertext = document.getElementById("ciphertext").value;
+	ciphertext = ciphertext.toLowerCase();
+	ciphertext = removeNonLowercase(ciphertext);
+	
+	//get frequencies
+	let frequencies = frequencyAnalysis(ciphertext, 1);
+	//frequencies = sortFrequenciesAlpha(frequencies);
+
+	let proportions = [];
+	let labels = [];
+
+	//(initialize lists)
+	for(let i = 0; i < 26; i++){
+		proportions.push(0);
+		labels.push(String.fromCharCode(i + 97));
+	}
+	
+	//(fill proportions)
+	for(let i = 0; i < frequencies.length; i++){
+		let index = frequencies[i].segment.charCodeAt(0) - 97;
+		proportions[index] = frequencies[i].proportion * 100;
+	}
+	
+	//clear canvas
+	let cnv = document.getElementById("chart-container");
+	let ctx = cnv.getContext("2d");
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0,0, cnv.height, cnv.width);
+	
+	let bar = new RGraph.Bar({
+        id: 'chart-container',
+        data: proportions,
+        options: {
+            labels: labels,
+			//textAccessible: false,
+            unitsPost: '%',
+            shadow: false,
+			tooltips: proportions,
+			highlightFill: "rgba(0,0,0,0.5)",
+            colors: ['red', 'pink'],
+            strokestyle: 'rgba(0,0,0,0)',
+            textSize: 10,
+            title: 'Single Letter Frequency Analysis',
+            numyticks: 8,
+            gutterLeft: 30,
+			gutterTop: 40,
+			crosshairs: true,
+			hmargin: 0,
+			zoomFactor: 0.001
+        }                
+    }).draw();
+	
+}
+
+
+function updateGraphSVG(){
+	
+	//sanitize input
+	let ciphertext = document.getElementById("ciphertext").value;
+	ciphertext = ciphertext.toLowerCase();
+	ciphertext = removeNonLowercase(ciphertext);
+	
+	//get frequencies
+	let frequencies = frequencyAnalysis(ciphertext, 1);
+	//frequencies = sortFrequenciesAlpha(frequencies);
+
+	let proportions = [];
+	let labels = [];
+
+	//(initialize lists)
+	for(let i = 0; i < 26; i++){
+		proportions.push(0);
+		labels.push(String.fromCharCode(i + 97));
+	}
+	
+	//(fill proportions)
+	for(let i = 0; i < frequencies.length; i++){
+		let index = frequencies[i].segment.charCodeAt(0) - 97;
+		proportions[index] = frequencies[i].proportion * 100;
+	}
+	
+	//clear svg
+	$("svg").empty();
+	
+	
+	let bar = new RGraph.SVG.Bar({
+		id: 'chart-container',
+		data: proportions,
+		options: {
+			hmargin: 1,
+			xaxisLabels: labels,
+			tooltips: proportions,
+			title: 'Single Character Frequency Analysis',
+			colors: ['red'],
+			shadow: true,
+			shadowOpacity: 0.5,
+			
+			xaxisLabelsOffsetx: 0,
+			xaxisLabelsOffsety: 0,
+			yaxisLabelsOffsetx: 0,
+			yaxisLabelsOffsety: 0
+		}
+	}).draw();
+	
+}
+
+
+//calculate frequencies
+singleFreqBtn = document.getElementById("btn-freq-single");
+doubleFreqBtn = document.getElementById("btn-freq-double");
+tripleFreqBtn = document.getElementById("btn-freq-triple");
+
+singleFreqBtn.addEventListener("click", function(){
+	
+	//sanitize input
+	let ciphertext = document.getElementById("ciphertext").value;
+	ciphertext = ciphertext.toLowerCase();
+	ciphertext = removeNonLowercase(ciphertext);
+	
+	//get frequencies
+	let frequencies = frequencyAnalysis(ciphertext, 1);
+	frequencies = sortFrequenciesProportion(frequencies);
+	
+	//display frequencies
+	let boxText = "";
+	for(let i = 0; i < frequencies.length; i++){
+		
+		let line = "";
+		line += frequencies[i].segment;
+		line += ":\t";
+		line += frequencies[i].count;
+		line += "\t";
+		line += frequencies[i].proportion;
+		
+		boxText += line;
+		
+		if(i < frequencies.length - 1){
+			boxText += "\n";
+		}
+		
+	}
+	
+	let box = document.getElementById("freq-box");
+	box.value = boxText;
+	
+	updateGraphSVG();
+	
+});
+
+doubleFreqBtn.addEventListener("click", function(){
+	
+	//sanitize input
+	let ciphertext = document.getElementById("ciphertext").value;
+	ciphertext = ciphertext.toLowerCase();
+	ciphertext = removeNonLowercase(ciphertext);
+	
+	//get frequencies
+	let frequencies = frequencyAnalysis(ciphertext, 2);
+	frequencies = sortFrequenciesProportion(frequencies);
+	
+	//display frequencies
+	let boxText = "";
+	for(let i = 0; i < frequencies.length; i++){
+		
+		let line = "";
+		line += frequencies[i].segment;
+		line += ":\t";
+		line += frequencies[i].count;
+		line += "\t";
+		line += frequencies[i].proportion;
+		
+		boxText += line;
+		
+		if(i < frequencies.length - 1){
+			boxText += "\n";
+		}
+		
+	}
+	
+	let box = document.getElementById("freq-box");
+	box.value = boxText;
+	
+	updateGraphSVG();
+	
+});
+
+tripleFreqBtn.addEventListener("click", function(){
+	
+	//sanitize input
+	let ciphertext = document.getElementById("ciphertext").value;
+	ciphertext = ciphertext.toLowerCase();
+	ciphertext = removeNonLowercase(ciphertext);
+	
+	//get frequencies
+	let frequencies = frequencyAnalysis(ciphertext, 3);
+	frequencies = sortFrequenciesProportion(frequencies);
+	
+	//display frequencies
+	let boxText = "";
+	for(let i = 0; i < frequencies.length; i++){
+		
+		let line = "";
+		line += frequencies[i].segment;
+		line += ":\t";
+		line += frequencies[i].count;
+		line += "\t";
+		line += frequencies[i].proportion;
+		
+		boxText += line;
+		
+		if(i < frequencies.length - 1){
+			boxText += "\n";
+		}
+		
+	}
+	
+	let box = document.getElementById("freq-box");
+	box.value = boxText;
+	
+	updateGraphSVG();
+	
+});
+
+
 
 //quick mod function
 function mod(a, b){
@@ -97,9 +328,9 @@ function mod(a, b){
 //bringing from old project as reference but probably just going to use .toLowerCase() when possible
 function removeNonLowercase(str){
 
-    var output = "";
+    let output = "";
     
-    for(var i = 0; i < str.length; i++){
+    for(let i = 0; i < str.length; i++){
         if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
             output += str.charAt(i);
         }
@@ -111,9 +342,9 @@ function removeNonLowercase(str){
 
 function factor(num){
 
-    var factorList = [];
+    let factorList = [];
 
-    for(var i = 2; i < num / 2.0; i++){
+    for(let i = 2; i < num / 2.0; i++){
         if(mod(num, i) == 0){
             factorList.push(i);
         }
@@ -129,12 +360,12 @@ function getDistances(text, maxLength){
     //utility function for checking if a substring exists in output
     function substrExists(outputArr, substring, substringIndex){
 
-        var result = false;
+        let result = false;
 
         //for every element in the output array
-        for(var i = 0; i < outputArr.length && !(result); i++){
+        for(let i = 0; i < outputArr.length && !(result); i++){
 
-            var obj = outputArr[i];
+            let obj = outputArr[i];
 
             //if the string is larger than the substring, check if the substring exists in it
             if(obj.str.length > substring.length){
@@ -156,19 +387,19 @@ function getDistances(text, maxLength){
     //-------------------------------------------------------------------//
 
 
-    var output = [];
+    let output = [];
 
     //start at maxLength, and go up to trigrams
-    for(var gramLength = maxLength; gramLength >= 3; gramLength--){
+    for(let gramLength = maxLength; gramLength >= 3; gramLength--){
 
         //for every possible position in the text
-        for(var textPos = 0; textPos <= text.length - gramLength; textPos++){
+        for(let textPos = 0; textPos <= text.length - gramLength; textPos++){
 
             //get the gram
-            var gram = text.substr(textPos, gramLength);
+            let gram = text.substr(textPos, gramLength);
 
             //look for match until one is found or until the string runs out
-            var testPos = textPos + gramLength;
+            let testPos = textPos + gramLength;
             while((testPos <= text.length - gramLength) && (text.substr(testPos, gramLength) != gram)){
                 testPos++;
             }
@@ -180,8 +411,8 @@ function getDistances(text, maxLength){
                 if(!substrExists(output, gram, textPos)){
 
                     //calculate distance and add gram to output
-                    var dist = testPos - textPos;
-                    var data = {
+                    let dist = testPos - textPos;
+                    let data = {
                         str: gram,
                         distance: dist,
                         startInd: textPos
@@ -207,11 +438,11 @@ function addFactors(distances){
 
     function isInFactors(factorList, fact){
 
-        var result = -1;
+        let result = -1;
 
-        var keepGoing = true;
+        let keepGoing = true;
         //for every factor object in factorList, look for the factor.
-        for(var i = 0; i < factorList.length && keepGoing; i++){
+        for(let i = 0; i < factorList.length && keepGoing; i++){
             //if you find it, return the index
             if(fact == factorList[i].factor){
                 result = i;
@@ -230,25 +461,25 @@ function addFactors(distances){
 
 
 
-    var output = [];
+    let output = [];
 
     //for every entry in distances
-    for(var i = 0; i < distances.length; i++){
+    for(let i = 0; i < distances.length; i++){
 
         //get the factorization
-        var factors = factor(distances[i].distance);
+        let factors = factor(distances[i].distance);
 
 
         //add the factors to output
-        for(var j = 0; j < factors.length; j++){
+        for(let j = 0; j < factors.length; j++){
 
             //if it's already in the list, add
-            var index = isInFactors(output, factors[j]);
+            let index = isInFactors(output, factors[j]);
 
             if(index != -1){
                 output[index].instances++;
             }else{
-                var obj = {
+                let obj = {
                     factor: factors[j],
                     instances: 1
                 };
@@ -264,13 +495,13 @@ function addFactors(distances){
 
 function sortFactorList(factors){
 
-    var i = 1;
+    let i = 1;
     while(i < factors.length){
-        var j = i;
+        let j = i;
         while(j > 0 && factors[j - 1].factor > factors[j].factor){
 
             //swap
-            var temp = factors[j];
+            let temp = factors[j];
             factors[j] = factors[j - 1];
             factors[j - 1] = temp;
 
@@ -285,12 +516,12 @@ function sortFactorList(factors){
 
 function segmentMessage(msg, keyLength){
 
-    var output = [];
+    let output = [];
 
-    for(var i = 0; i < keyLength; i++){
+    for(let i = 0; i < keyLength; i++){
 
         output[i] = "";
-        var letter = i;
+        let letter = i;
 
         while(letter < msg.length){
             output[i] += msg.substr(letter, 1);
@@ -306,9 +537,9 @@ function segmentMessage(msg, keyLength){
 //bringing from old project as reference but probably just going to use .toLowerCase() when possible
 function removeNonLowercase(str){
 
-    var output = "";
+    let output = "";
 
-    for(var i = 0; i < str.length; i++){
+    for(let i = 0; i < str.length; i++){
         if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
             output += str.charAt(i);
         }
@@ -329,18 +560,18 @@ function getCaesarIC(){
     segments = parseInt(segments, 10);
 
     //get ciphertext
-    var ciphertext = caesar_IC_input_box.value;
+    let ciphertext = caesar_IC_input_box.value;
     ciphertext = ciphertext.toLowerCase();
     ciphertext = removeNonLowercase(ciphertext);
 
     //get segments
-    var seg = segmentMessage(ciphertext, segments);
+    let seg = segmentMessage(ciphertext, segments);
 
     //for each segment, get IC
-    var ICText = "";
-    for(var i = 0; i < seg.length; i++) {
+    let ICText = "";
+    for(let i = 0; i < seg.length; i++) {
 
-        var IC = indexOfCoincidence(seg[i]);
+        let IC = indexOfCoincidence(seg[i]);
         ICText += "[" + (i + 1) + "]  ";
         ICText += IC;
         ICText += '\r\n';
@@ -358,18 +589,18 @@ function getKeywordIC(){
     let segments = keyword_ic_input.value
     segments = parseInt(segments, 10);
     //get ciphertext
-    var ciphertext = keyword_IC_input_box.value;
+    let ciphertext = keyword_IC_input_box.value;
     ciphertext = ciphertext.toLowerCase();
     ciphertext = removeNonLowercase(ciphertext);
 
     //get segments
-    var seg = segmentMessage(ciphertext, segments);
+    let seg = segmentMessage(ciphertext, segments);
 
     //for each segment, get IC
-    var ICText = "";
-    for(var i = 0; i < seg.length; i++) {
+    let ICText = "";
+    for(let i = 0; i < seg.length; i++) {
 
-        var IC = indexOfCoincidence(seg[i]);
+        let IC = indexOfCoincidence(seg[i]);
         ICText += "[" + (i + 1) + "]  ";
         ICText += IC;
         ICText += '\r\n';
@@ -387,18 +618,18 @@ function getVigenereIC(){
     let segments = vigenere_ic_input.value
     segments = parseInt(segments, 10);
     //get ciphertext
-    var ciphertext = vigenere_IC_input_box.value;
+    let ciphertext = vigenere_IC_input_box.value;
     ciphertext = ciphertext.toLowerCase();
     ciphertext = removeNonLowercase(ciphertext);
 
     //get segments
-    var seg = segmentMessage(ciphertext, segments);
+    let seg = segmentMessage(ciphertext, segments);
 
     //for each segment, get IC
-    var ICText = "";
-    for(var i = 0; i < seg.length; i++) {
+    let ICText = "";
+    for(let i = 0; i < seg.length; i++) {
 
-        var IC = indexOfCoincidence(seg[i]);
+        let IC = indexOfCoincidence(seg[i]);
         ICText += "[" + (i + 1) + "]  ";
         ICText += IC;
         ICText += '\r\n';
@@ -416,18 +647,18 @@ function getAnagramIC(){
     let segments = anagram_ic_input.value
     segments = parseInt(segments, 10);
     //get ciphertext
-    var ciphertext = anagram_IC_input_box.value;
+    let ciphertext = anagram_IC_input_box.value;
     ciphertext = ciphertext.toLowerCase();
     ciphertext = removeNonLowercase(ciphertext);
 
     //get segments
-    var seg = segmentMessage(ciphertext, segments);
+    let seg = segmentMessage(ciphertext, segments);
 
     //for each segment, get IC
-    var ICText = "";
-    for(var i = 0; i < seg.length; i++) {
+    let ICText = "";
+    for(let i = 0; i < seg.length; i++) {
 
-        var IC = indexOfCoincidence(seg[i]);
+        let IC = indexOfCoincidence(seg[i]);
         ICText += "[" + (i + 1) + "]  ";
         ICText += IC;
         ICText += '\r\n';
@@ -445,18 +676,18 @@ function getColumnarIC(){
     let segments = columnar_ic_input.value
     segments = parseInt(segments, 10);
     //get ciphertext
-    var ciphertext = columnar_IC_input_box.value;
+    let ciphertext = columnar_IC_input_box.value;
     ciphertext = ciphertext.toLowerCase();
     ciphertext = removeNonLowercase(ciphertext);
 
     //get segments
-    var seg = segmentMessage(ciphertext, segments);
+    let seg = segmentMessage(ciphertext, segments);
 
     //for each segment, get IC
-    var ICText = "";
-    for(var i = 0; i < seg.length; i++) {
+    let ICText = "";
+    for(let i = 0; i < seg.length; i++) {
 
-        var IC = indexOfCoincidence(seg[i]);
+        let IC = indexOfCoincidence(seg[i]);
         ICText += "[" + (i + 1) + "]  ";
         ICText += IC;
         ICText += '\r\n';
@@ -469,20 +700,20 @@ function indexOfCoincidence(message){
     message = message.toLowerCase();
     message = removeNonLowercase(message);
 
-    var N = message.length;
+    let N = message.length;
 
-    var sum = 0;
+    let sum = 0;
 
     //for every letter in the alphabet, get the amount of each letter,
     //then get n(n-1) and add to sum
-    for(var i = 0; i < 26; i++){
+    for(let i = 0; i < 26; i++){
 
         //get letter
-        var letter = String.fromCharCode(i + 97);
+        let letter = String.fromCharCode(i + 97);
 
         //count occurences
-        var occurences = 0;
-        for(var j = 0; j < N; j++){
+        let occurences = 0;
+        for(let j = 0; j < N; j++){
             if(message.charAt(j) == letter){
                 occurences++;
             }
@@ -494,7 +725,7 @@ function indexOfCoincidence(message){
 
 
 
-    var output = sum / (N * (N - 1));
+    let output = sum / (N * (N - 1));
 
     return output;
 
